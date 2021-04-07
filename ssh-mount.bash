d6c1f15f9e3c18699934450bbdf76b8a809710e7
Mark Page <mark.page@whitelamp.com>
2021-04-07 15:53:05 +0100

    Revises project to work under low-level user rather than root

--- ssh-mount.bash
+++ ssh-mount.bash
@@ -4,6 +4,7 @@ ps="$(ps ax)"
 found="$(echo "$ps" | grep "$(realpath "$0")" | grep -v "$match")"
 
 
+
 if [ "$found" ]
 then
     echo -n "$0 already running in another process - "
@@ -14,34 +15,42 @@ fi
 
 
 # Configure
-idFile="$1"
-remDir="$2"
-mntDir="$3"
-if [ ! "$3" ]
+remDir="$1"
+mntDir="$2"
+here="$(dirname "$(realpath $0)")"
+
+
+
+# Explain
+if [ ! "$2" ]
 then
     echo "Usage:"
-    echo "/bin/bash $0 pub_key_file remote_host:/path/ /mnt/directory [port def 22] [timeout_secs def 60, 0 = keep trying forever]"
-    echo "Example cron entries:"
-    echo "@reboot    /bin/bash /root/whitelamp-ssh-mount/ssh-mount.bash /home/mark/.ssh/id_rsa mark@snowy:/ /mnt/snowy > /var/log/sshfs-mount.boot.log 2>&1"
-    echo "* * * * *  /bin/bash /root/whitelamp-ssh-mount/ssh-mount.bash /home/mark/.ssh/id_rsa mark@snowy:/ /mnt/snowy > /var/log/sshfs-mount.minutely.log 2>&1"
+    echo "  /bin/bash $0 remote_host:/path/ /mnt/directory [port default 22] [timeout_secs default=10, 0=keep trying forever]"
+    echo "Example mount point:"
+    echo "  mkdir $HOME/myremote"
+    echo "Example crontab entries:"
+    echo "  @reboot    $USER  /bin/bash  $here/ssh-mount.bash  $USER@myremote:/  $HOME/myremote  > $HOME/sshfs-mount.myremote.log 2>&1"
+    echo "  * * * * *  $USER  /bin/bash  $here/ssh-mount.bash  $USER@myremote:/  $HOME/myremote  > $HOME/sshfs-mount.myremote.log 2>&1"
     exit
 fi
-port="$4"
+port="$3"
 if [ ! "$port" ]
 then
     port="22"
 fi
 host="$(echo $remDir | tr '@' ' ' | tr ':' ' ' | awk '{print $2;}')"
-toSecs="$5"
+toSecs="$4"
 if [ ! "$toSecs" ]
 then
-    toSecs="60"
+    toSecs="10"
 fi
 
 
 
+# Get started
 echo "$0 started"
 date '+%Y-%m-%dT%H:%M:%S'
+
 
 
 # Check if already mounted
@@ -56,19 +65,22 @@ fi
 
 # Wait for a network connection
 date '+%Y-%m-%dT%H:%M:%S'
-echo -n "$0 waiting for test connection to $host - "
+echo -n "$0 waiting for test connection to $host .."
 i=0
 while [ 1 ]
 do
+    echo -n "."
     i=$((i+1))
     if [ "$(echo "$(nc -zv $host $port 2>&1)" | grep succeeded)" ]
     then
+        echo -n " "
         date '+%Y-%m-%dT%H:%M:%S'
         echo "Successfully connected to $host"
         break;
     fi
     if [ "$i" = "$toSecs" ]
     then
+        echo -n " "
         date '+%Y-%m-%dT%H:%M:%S'
         echo "* $0 timed out on test connection to $host"
         exit
@@ -80,20 +92,18 @@ done
 
 # Mount SSHFS
 now=$(date '+%Y-%m-%dT%H:%M:%S')
-sudo sshfs -o allow_other,IdentityFile=$idFile $remDir $mntDir
+sshfs $remDir $mntDir
 err=$?
 
 
-# Report
+
+# Finish up
+echo $now
 if [ $err = 0 ]
 then
-    echo $now
     echo "* $0 mounted $remDir to $mntDir"
 else
-    echo $now
     echo "* $0 failed to mount $remDir to $mntDir [sshfs exit code $err]"
-    umount $mntDir
-    echo "Unmounted for a clean mount next time"
 fi
 
 
